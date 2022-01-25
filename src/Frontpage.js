9deffffa764429b71c0c71839b82f9c251b17744
TheirryEHB <thierry.klougbo@student.ehb.be>
2022-01-25 23:17:50 +0100

    Frontpage oke voor nu

--- src/Frontpage.js
+++ src/Frontpage.js
@@ -1,33 +1,47 @@
 
-import Header from "./partials/Header";
-import "./Frontpage.css";
 import React from 'react';
+import  {Button, Card}  from 'react-bootstrap';
+import "./Frontpage.css";
+import {FaLinkedin} from "react-icons/fa";
 
 
+function getAge(dateString) {
+    var today = new Date();
+    var birthDate = new Date(dateString);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+}
 
 function frontpage(){
 
     return(
         <div className="frontPage">
             <div className="grid-container1">
-                <div className="card whoIAm">
-                    <div className="imgDiv"></div>
-                        <div className="card-body">
-                            <h5 className="card-title">Who am I?</h5>
-                            <p className="card-text">This section will be about me. Who am I?
-                                Where do I come from? Etc...</p>
-                            <a href="https://www.linkedin.com/in/thierry-klougbo-880b071b1/" target="_blank"
-                               className="btn btn-primary">LinkedIn</a>
-                        </div>
-                </div>
+                <Card  className="card-body card whoIAm">
+                        <Card.Img className="imgDiv"/>
+                        <Card.Title className="card-title">Who am I?</Card.Title>
+                        <p className="card-text">As you may have noticed, my name is Thierry Klougbo. I am currently 22 years old, and originate from Togo (Africa)
+                            even though I have lived in Belgium for 13 years now.
+                            <br/>
+                            Currently I am an Applied Informatics student at the university college of Erasmus in Brussels. This goes to show that I am an aspiring
+                            Software Developer.
+                            After graduating -in 2023- I am planning on working and meanwhile study for another bachelor.
+                        </p>
+                        <Card.Link href="https://www.linkedin.com/in/thierry-klougbo-880b071b1/" target="_blank" className="btn btn-primary" style={{width:'10%', padding:'0'}}>
+                            <FaLinkedin style={{height:'2em', width:'2em'}}/>
+                        </Card.Link>
+                </Card>
 
-                <div className="card whatIsThis">
-                    <div className="card-body">
-                        <h5 className="card-title">What is this?</h5>
-                        <p className="card-text">This section will be about the website itself. Why am I making it. What
-                            have I learned while doing it...</p>
-                    </div>
-                </div>
+                <Card className="card-body card whatIsThis">
+                        <Card.Title className="card-title">So, what is this?</Card.Title>
+                        <p className="card-text">First I started making this portfolio by simply using HTML, CSS and JavaScript. After learning PHP in school
+                        I made another version using that too. But after realising how much React JS is asced on the jobmarket, I decided to make the final version using
+                            that.</p>
+                </Card>
             </div>
         </div>
     )
--- src/partials/Header.js
+++ src/partials/Header.js
@@ -3,6 +3,7 @@ import "./Header.css";
 import logoimg from '../images/portfolio logo.png';
 import React, {useState} from 'react';
 import { Link }from 'react-router-dom'
+import {FaHome, FaRaspberryPi} from "react-icons/fa";
 
 
 export default function Header(){
@@ -15,10 +16,10 @@ export default function Header(){
                 </figure>
                 <ul className="navibar">
                     <li className={"navitem"}>
-                        <Link className={"navlink"} to="/">Home</Link>
+                        <Link className={"navlink"} to="/"> <FaHome style={{marginRight: '10px', marginBottom: '5'}}/> Home</Link>
                     </li>
                     <li className={"navitem"}>
-                        <Link className={"navlink"} to="/projects">Projects</Link>
+                        <Link className={"navlink"} to="/projects"> <FaRaspberryPi style={{marginRight: '10px', marginBottom: '5'}}/> Projects</Link>
                     </li>
                 </ul>
             </nav>
