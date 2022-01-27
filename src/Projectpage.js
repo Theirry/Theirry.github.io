dc999335d618141a9091f97781f6ec7938698b42
TheirryEHB <thierry.klougbo@student.ehb.be>
2022-01-27 21:15:57 +0100

    fix

--- src/Projectpage.js
+++ src/Projectpage.js
@@ -1,13 +1,12 @@
 
 import React from 'react';
-import Header from "./partials/Header";
 
 function Projectpage(){
     return(
-        <div className="Projectpage">
-            <h1>Here</h1>
-            <h1>This is the project page</h1>
+        <div>
+
         </div>
+
     )
 }
 
