84c4bd3c204c4cb8b08a8ab3d4aee62cb07e1e1e
TheirryEHB <thierry.klougbo@student.ehb.be>
2022-04-10 09:15:48 +0200

    Veiligheids commit

--- src/Pricing.js
+++ src/Pricing.js
@@ -2,7 +2,7 @@
 import React, {useRef, useState} from 'react';
 import {MdOutlineArrowBackIosNew, MdOutlineArrowForwardIos, MdEuro} from "react-icons/md"
 import {Button, Card}  from 'react-bootstrap';
-import {motion} from 'framer-motion';
+import {motion, AnimatePresence} from 'framer-motion';
 import "./Pricing.css"
 
 // Freelance front-end dev: https://sanderlangendoen.nl/tarieven-freelance-frontend-ontwikkelaar/
@@ -34,8 +34,13 @@ const cardArray = [priceR1, priceR2, priceR3];
 
 
 const setSelected = (valueProp) => {
-    console.log(`$pr${valueProp}`)
-    document.getElementById(`pr${valueProp}`).style.transform = "scale(1.05, 1.05)"
+    const el = document.getElementById(`pr${valueProp}`)
+    el.style.transform = "scale(1.05, 1.05)"
+
+  /*  el.setAttribute( "text-shadow", "0 0 3px #FF0000")
+    el.setAttribute( "animate", "scale: 1.05")
+    el.setAttribute( "transition", "duration: 2")*/
+
 }
 
 const resetSelected = () => {
@@ -56,9 +61,8 @@ function Pricing(){
                     //     pri.setAttribute("margin", "0 5em 0 0")
                     //     pri.setAttribute( "overflow", "hidden")
                     // }
-                    console.log(_selected)
+                    console.log(`pr${_selected}`)
                     if (_selected === 1) {
-                        _setSelected(1)
                         resetSelected()
                         setSelected(_selected)
                     }
@@ -75,7 +79,9 @@ function Pricing(){
                     <h1 style={{margin: "0.5em 0 1.25em 0"}}>Standard Pricing</h1>
                     <p></p>
 
-                    <ul id="pricelist" className={"grid-container3"} style={{height:"80%"}}>
+                    <motion.ul
+                        id="pricelist" className={"grid-container3"} style={{height:"80%"}}
+                    >
                         {
                             cardArray.map(item =>
                             <motion.li id={item.id} key={item.id} class={"listitem"}>
@@ -86,7 +92,7 @@ function Pricing(){
                             </motion.li>
                         )
                         }
-                    </ul>
+                    </motion.ul>
                 </div>
 
                 <div className={"arrowRight"} onClick={() => {
@@ -103,6 +109,28 @@ function Pricing(){
                     <MdOutlineArrowForwardIos size={70} className={"arrNext"}/>
                 </div>
             </div>
+
+           {/* <AnimatePresence>
+                {_selected && (
+                    cardArray.map(item => (
+                        <motion.li
+                            id={item.id}
+                            class={"listitem"}
+
+                            key={item.id}
+                            initial={{scale: 1}}
+                            animate={{scale: 1.05}}
+                            transition={{duration: 0.5}}
+                            exit={{scale: 1}}
+                        >
+                            <motion.div className={"pricerange"} id={"pricerange"+item.id}>
+                                <motion.div className={"class-title ct"}>{item.title}</motion.div>
+                                <motion.h4 className={"h4cards"}>{<MdEuro/>}{item.price1} - {<MdEuro/>}{item.price2}/ hour</motion.h4>
+                            </motion.div>
+                        </motion.li>
+                        )
+                    ))}
+            </AnimatePresence>*/}
         </div>
     )
 }
