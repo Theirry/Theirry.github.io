6747ff26dd759b156c8e62234d03d0c8dbe80728
Theirry <thierry.klougbo@student.ehb.be>
2022-04-13 18:19:04 +0200

    Updates

---
+++ precache-manifest.9b83690e115a3ff552ad7f586edfa462.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f4cd202bf69da2df1d9901f0a5f0e4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a499f594625368efd60",
+    "url": "/static/css/2.f71ce0dd.chunk.css"
+  },
+  {
+    "revision": "bd01c13602cd79ef7372",
+    "url": "/static/css/main.149c655e.chunk.css"
+  },
+  {
+    "revision": "8a499f594625368efd60",
+    "url": "/static/js/2.fb20dc8c.chunk.js"
+  },
+  {
+    "revision": "171784eaa8c1c772844b7f8274561078",
+    "url": "/static/js/2.fb20dc8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd01c13602cd79ef7372",
+    "url": "/static/js/main.a9446566.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "ee079982493f426954c0a5cdb7f2b182",
+    "url": "/static/media/front-black-xl_1x.ee079982.png"
+  },
+  {
+    "revision": "3dd37d6db47ac9723f099020f141018f",
+    "url": "/static/media/mepic4_remove_colored-removebg-preview.3dd37d6d.png"
+  },
+  {
+    "revision": "2b82f67362ffb844ea497670047c7283",
+    "url": "/static/media/portfolio logo.2b82f673.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.74f44e15765a97a2ffd70d65b4ac1ed8.js"
+  "/precache-manifest.9b83690e115a3ff552ad7f586edfa462.js"
 );
 
 self.addEventListener('message', (event) => {
